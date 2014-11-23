39e5db013e698507c1a8e9c3559a8cd786f5d967
Matthew Platts <plattsmw@gmail.com>
2014-11-23 13:10:13 +1100

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ jquery-ajax-progress-bar
 ========================
 
 Simple ajax top loading bar like on Youtube. No setup needed. Just include js and css files on your site. Uses $.ajaxStart and $.ajaxComplete callbacks.
+
+(Demo)[http://www.webtempest.com] - navigate around the site and look up the top
+
+To change color just edit the css.
+
+Modified from (peachananr/loading-bar)[https://github.com/peachananr/loading-bar]
---
+++ jquery-ajax-progress-bar.js
@@ -0,0 +1,15 @@
+(function (){
+  $(document).ajaxStart( function () {
+    if ($("#loadingbar").length === 0) {
+      $("body").append("<div id='loadingbar'></div>")
+      $("#loadingbar").addClass("waiting").append($("<dt/><dd/>"));
+      $("#loadingbar").width((50 + Math.random() * 30) + "%");
+    }
+  })
+
+  $(document).ajaxComplete(function () {
+    $("#loadingbar").width("101%").delay(200).fadeOut(400, function () {
+      $(this).remove();
+    });
+  })
+})();
